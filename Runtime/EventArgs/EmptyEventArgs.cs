ec3b30a4823e0fe86c6df30e3fc6dfe457a35c7f
Blank <wangfj11@foxmail.com>
2025-04-27 15:29:04 +0800

    [增加]1. 增加发送空事件的函数接口

---
+++ Runtime/EventArgs/EmptyEventArgs.cs
@@ -0,0 +1,48 @@
+﻿// GameFrameX 组织下的以及组织衍生的项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// 
+// 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE 文件。
+// 
+// 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+
+using GameFrameX.Runtime;
+
+namespace GameFrameX.Event.Runtime
+{
+    /// <summary>
+    /// 空事件
+    /// </summary>
+    public sealed class EmptyEventArgs : GameEventArgs
+    {
+        private static string _eventId = typeof(EmptyEventArgs).FullName;
+
+        public override void Clear()
+        {
+        }
+
+        public override string Id
+        {
+            get { return _eventId; }
+        }
+
+        /// <summary>
+        /// 创建空事件
+        /// </summary>
+        /// <param name="eventId">事件编号</param>
+        /// <returns>空事件对象</returns>
+        public static EmptyEventArgs Create(string eventId)
+        {
+            var eventArgs = ReferencePool.Acquire<EmptyEventArgs>();
+            eventArgs.SetEventId(eventId);
+            return eventArgs;
+        }
+
+        /// <summary>
+        /// 设置事件编号
+        /// </summary>
+        /// <param name="eventId"></param>
+        private void SetEventId(string eventId)
+        {
+            _eventId = eventId;
+        }
+    }
+}
