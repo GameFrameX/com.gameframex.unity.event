8f4ea3eac38c126ec234db7cd084d6209f9e4a45
Blank <wangfj11@foxmail.com>
2025-04-27 17:06:11 +0800

    [增加]1. 增加发送空事件的函数接口 https://github.com/GameFrameX/GameFrameX.Unity/issues/23

--- Runtime/EventComponent.cs
+++ Runtime/EventComponent.cs
@@ -133,6 +133,17 @@ namespace GameFrameX.Event.Runtime
         }
 
         /// <summary>
+        /// 抛出事件，这个操作是线程安全的，即使不在主线程中抛出，也可保证在主线程中回调事件处理函数，但事件会在抛出后的下一帧分发。
+        /// </summary>
+        /// <param name="sender">事件发送者。</param>
+        /// <param name="eventId">事件编号。</param>
+        public void Fire(object sender, string eventId)
+        {
+            GameFrameworkGuard.NotNullOrEmpty(eventId, nameof(eventId));
+            m_EventManager.Fire(sender, EmptyEventArgs.Create(eventId));
+        }
+
+        /// <summary>
         /// 抛出事件立即模式，这个操作不是线程安全的，事件会立刻分发。
         /// </summary>
         /// <param name="sender">事件发送者。</param>
